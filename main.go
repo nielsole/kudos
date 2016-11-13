ca9c8cb38850da664fa5f8c69babee08003fa4d4
Niels-Ole Kühl <nielsole.kuehl@gmail.com>
2016-11-13 22:24:02 +0100

    The production version

--- main.go
+++ main.go
@@ -3,34 +3,54 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
+	"fmt"
+	"gopkg.in/redis.v5"
 	"strconv"
 )
 
-func handler1(w http.ResponseWriter, r *http.Request, counter Counter){
+func createRedisConnection() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	return client
+}
+
+func handler1(w http.ResponseWriter, r *http.Request, conn *redis.Client) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	if r.Method == "GET"{
-		//rUrl := r.URL.Query().Get("url")
+	rUrl := r.URL.Query().Get("url")
+	if r.Method == "GET" {
 		//counter.count++
-		w.Write([]byte(strconv.Itoa(counter.count)))
-	}else{
-		counter.count++
-		w.Write([]byte(strconv.Itoa(counter.count)))
+		counter, err := conn.Get(rUrl).Result()
+		if err == redis.Nil {
+			counter = "0"
+		} else if err != nil {
+			println("Something went wrong while retrieving the current value", counter, err)
+		}
+		w.Write([]byte(counter))
+	} else {
+		counter, err := conn.Incr(rUrl).Result()
+		if err != nil {
+			println(err)
+		}
+		w.Write([]byte(strconv.FormatInt(counter, 10)))
 	}
 }
 
-type Counter struct{
-	count int
-}
-
-func main(){
-
-	counter := Counter{0}
+func main() {
+	conn := createRedisConnection()
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		handler1(w,r,counter)
+		handler1(w, r, conn)
 	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
-	http.Handle("/",r)
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil{
+		println(err)
+	}
 
 }
