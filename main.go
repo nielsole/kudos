1aecfbf55c34c47b3e0c8924ef6bf576ce0cc4f8
Niels-Ole Kühl <nielsole.kuehl@gmail.com>
2017-01-30 19:40:15 +0100

    Dirty implementation of per-domain counts

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func createRedisConnection() *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
@@ -22,6 +22,47 @@ func createRedisConnection() *redis.Client {
 func handler1(w http.ResponseWriter, r *http.Request, conn *redis.Client) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	rUrl := r.URL.Query().Get("url")
+	if rUrl == ""{
+		domain := r.URL.Query().Get("domain")
+		if domain == ""{
+			w.WriteHeader(400)
+			w.Write([]byte("Use either 'url' or 'domain' as GET parameters"))
+			return
+		}
+		//else
+		if r.Method != "GET"{
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		kudos := uint64(0)
+		var keys []string
+		var cursor uint64 = 0
+		var err error
+		for{
+			println("Scanning...", cursor, kudos)
+			scanRes := conn.Scan(cursor,"*//" + domain + "/*", 100)
+			keys, cursor, err = scanRes.Result()
+			if err != nil{
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			for _, key := range keys{
+				println("Retrieving individual url", key)
+				urlKudos, err1 := conn.Get(key).Result()
+				if err1 != nil{
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				kudosIncr, _ := strconv.ParseUint(urlKudos, 10, 64)
+				kudos += kudosIncr
+			}
+			if cursor == 0{
+				break
+			}
+		}
+		w.Write([]byte(strconv.FormatUint(kudos, 10)))
+		return
+	}
 	if r.Method == "GET" {
 		//counter.count++
 		counter, err := conn.Get(rUrl).Result()
@@ -45,6 +86,7 @@ func main() {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		handler1(w, r, conn)
 	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
 	http.Handle("/", r)
